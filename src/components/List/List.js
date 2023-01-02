cc41082f3ca9bf7919e25eb0218fd93b4198335e
KamilKorczek <korczek@gmail.com>
2023-01-02 21:01:43 +0100

    Added List component and global styles

---
+++ src/components/List/List.js
@@ -0,0 +1,32 @@
+const List = () => {
+  return (
+    <div>
+        <header>
+            <h2>Calculate batch size</h2>
+        </header>
+        <p>Chemical products useful at home and more</p>
+        <section>
+            <article>
+                <h2>SOLVENTS</h2>
+            </article>
+            <article>
+                <h2>ADHESIVES</h2>
+            </article>
+            <article>
+                <h2>HOUSEHOLD CHEMICAL TECHNICAL</h2>
+            </article>
+            <article>
+                <h2>PAINTS</h2>
+            </article>
+            <article>
+                <h2>PREPARATION FOR PAINTING AND GLUING</h2>
+            </article>
+            <article>
+                <h2>SEALANTS</h2>
+            </article>
+        </section>
+    </div>
+  );
+};
+
+export default List;
